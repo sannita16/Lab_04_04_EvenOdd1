504342c8ac6398e8d771704af33186d5a5c9401c
sannita16 <sannita.mail.uc.edu>
2026-09-17 15:38:35 -0400

    Initial commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd
+{
+    static void main()
+    {
+        int valToTest = 7;
+        IO.println(" If val mod 2 = 0 then it is even");
+        IO.println("If val mod 2 =1 then it is odd");
+
+        IO.println(valToTest % 2);
+    }
+}
